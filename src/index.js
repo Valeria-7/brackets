95fe08124c8aa4780c6408cf3019df8c9b127b76
Valeria Evseenkova <evseenkovavaleria2001@mail.ru>
2022-07-26 21:01:40 +0300

    feat: implement check function

--- src/index.js
+++ src/index.js
@@ -1,56 +1,33 @@
 module.exports = function check(str, bracketsConfig) {
-  console.log(str)
 
-  // получить массив открывающихся скобок openBrackets ['(', '{']
   // получить объект соответствий bracketsPair [')']:'('
-
-  let openBrackets = []  // '|'
-  const bracketsPair = {} // '|':'|'
+  const bracketsPair = {} 
   for (let i = 0; i < bracketsConfig.length; i++) {
-    openBrackets.push(bracketsConfig[i][0])
     bracketsPair[bracketsConfig[i][1]] = bracketsConfig[i][0]
   }
 
+  // создать стек
   let stack = []
 
+  // перебрать исходную строку
   for (let i = 0; i < str.length; i++) {
+    // запомнить текущий элемент и верхний в стеке
     let currentSymbol = str[i]
+    let topElement = stack[stack.length - 1]
 
-    if (openBrackets.includes(currentSymbol)) {
+    // первый элемент записать в любом случае
+    if (stack.length === 0) {
       stack.push(currentSymbol)
     } else {
-      if (stack.length == 0) {
-        return false
-      }
-
-      let topElement = stack[stack.length - 1]
-
-      if (bracketsPair[currentSymbol] == topElement) {
+      // если скобка закрывает верхнюю в стеке - удалить
+      if (bracketsPair[currentSymbol] === topElement) {
         stack.pop()
       } else {
-        return false
+        // если нет - добавить в стек
+        stack.push(currentSymbol)
       }
     }
-
   }
+  // Вернуть true если стек пустой - все скобки закрыты, иначе false
   return stack.length === 0
 }
-
-//console.log(check('||', [['|', '|']]))
-
-/*
-check('()', [['(', ')']]) // -> true
-check('((()))()', [['(', ')']]) // -> true
-check('())(', [['(', ')']]) // -> false
-check('([{}])', [['(', ')'], ['[', ']'], ['{', '}']]) // -> true
-check('[(])', [['(', ')'], ['[', ']']]) // -> false
-check('[]()', [['(', ')'], ['[', ']']]) // -> true
-check('[]()(', [['(', ')'], ['[', ']']]) // -> false
-
-// special case: opening and closing bracket can be the same :)
-
-check('||', [['|', '|']]) // -> true
-check('|()|', [['(', ')'], ['|', '|']]) // -> true
-check('|(|)', [['(', ')'], ['|', '|']]) // -> false
-check('|()|(||)||', [['(', ')'], ['|', '|']]) // -> true
- */
